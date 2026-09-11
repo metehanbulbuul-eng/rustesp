78a20385784dc806a1df34fa007886183b295136
metehanbulbuul-eng <metehanbulbuul@gmail.com>
2026-09-11 18:06:44 +0300

    Remove libcxx dependency, use NDK STL

--- module/jni/Android.mk
+++ module/jni/Android.mk
@@ -6,7 +6,4 @@ LOCAL_SRC_FILES := main.cpp il2cpp.cpp gl.cpp
 LOCAL_CXXFLAGS := -std=c++17 -O2 -fvisibility=hidden -fvisibility-inlines-hidden -Wall -Wno-unused-variable -Wno-unused-function
 LOCAL_LDFLAGS := -Wl,--gc-sections -Wl,--exclude-libs,ALL
 LOCAL_LDLIBS := -llog -lEGL -lGLESv3 -landroid
-LOCAL_STATIC_LIBRARIES := cxx
 include $(BUILD_SHARED_LIBRARY)
-
-include $(LOCAL_PATH)/libcxx/Android.mk
--- module/jni/Application.mk
+++ module/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := arm64-v8a
 APP_PLATFORM := android-29
-APP_STL := none
+APP_STL := c++_static
 APP_CFLAGS := -Oz
 APP_CPPFLAGS := -std=c++17 -Oz
 APP_ALLOW_MISSING_DEPS := true
