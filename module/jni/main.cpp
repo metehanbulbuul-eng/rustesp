455ea40340a09a79109c07a0770c7b2e0a78cfd0
metehanbulbuul-eng <metehanbulbuul@gmail.com>
2026-09-17 02:54:55 +0300

    fix: remove unsafe egl hook stub to fix black screen and stabilize module

--- module/jni/main.cpp
+++ module/jni/main.cpp
@@ -38,48 +38,11 @@ Il2CppClass* g_entity_manager_class = nullptr;
 
 static bool g_is_target = false;
 static bool g_gl_ready = false;
-static std::atomic<bool> g_esp_on{true}; // Test için açık başlasın
+static std::atomic<bool> g_esp_on{true};
 static std::atomic<int> g_egl_count{0};
 static int g_screen_w = 1080, g_screen_h = 2400;
 
 #define ESP_STATE_FILE "/data/local/tmp/esp_state.txt"
-
-// EGL hook için pointer (Dobby olmadığı için şimdilik sadece log basıyoruz)
-typedef EGLBoolean (*eglSwapBuffers_t)(EGLDisplay, EGLSurface);
-static eglSwapBuffers_t g_orig_egl = nullptr;
-
-// Güvenli EGL SwapBuffers simülasyonu / callback tetikleyicisi
-static EGLBoolean my_eglSwapBuffers(EGLDisplay dpy, EGLSurface surf) {
-    int c = g_egl_count.fetch_add(1) + 1;
-    if (c == 1) LOGI(">>> eglSwapBuffers ILK CAGRI (Grafik Devrede) <<<");
-
-    if (!g_gl_ready) {
-        EGLint w = 0, h = 0;
-        eglQuerySurface(dpy, surf, EGL_WIDTH, &w);
-        eglQuerySurface(dpy, surf, EGL_HEIGHT, &h);
-        if (w > 0 && h > 0) {
-            g_screen_w = w;
-            g_screen_h = h;
-            if (gl_init()) {
-                g_gl_ready = true;
-                LOGI("GL init OK: %dx%d", w, h);
-            }
-        }
-    }
-
-    if (g_gl_ready) {
-        glViewport(0, 0, g_screen_w, g_screen_h);
-        glDisable(GL_DEPTH_TEST);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        // Test amaçlı ekrana küçük bir kutu çizelim (Modülün çizim yapıp yapmadığını görmek için)
-        gl_draw_filled_rect(50, 50, 100, 100, 0, 255, 0, 255);
-    }
-
-    // Orijinal fonksiyon olmadığı için direkt EGL_TRUE dönüyoruz (oyun çökmesin diye)
-    return EGL_TRUE; 
-}
 
 class RustESPModule : public ModuleBase {
 public:
@@ -111,16 +74,12 @@ public:
     void postAppSpecialize(const AppSpecializeArgs*) override {
         if (!g_is_target) return;
 
-        // Oyun açılışını bloklamamak için her şeyi arka plan iş parçacığına (thread) alıyoruz
+        // Oyun açılışını bloklamamak ve siyah ekranı önlemek için arka plan thread'i
         std::thread([]() {
             LOGI("Arka plan baslatildi, libil2cpp.so bekleniyor...");
             int a = 0;
             
-            // libil2cpp.so'nun belleğe yüklenmesi için max 60 saniye bekleyelim
-            uintptr_t il2cpp_base = 0;
             while (a < 60) {
-                // il2cpp.cpp içerisindeki fonksiyonu tetiklemek için base kontrolü
-                // Basit bir dlopen kontrolü yapalım
                 void* handle = dlopen("libil2cpp.so", RTLD_NOLOAD);
                 if (handle) {
                     dlclose(handle);
@@ -143,7 +102,6 @@ public:
 
             LOGI("Il2cpp API basariyla hazir!");
             
-            // Sınıf bulma denemeleri (Çökme yapmaması için kontrollü)
             g_player_class = find_class("WizardGames.Soc.Common.Entity", "PlayerEntity");
             g_camera_class = find_class("UnityEngine", "Camera");
             g_entity_manager_class = find_class("WizardGames.Soc.Share.Framework", "EntityManager");
@@ -151,7 +109,6 @@ public:
             LOGI("Bulunan Siniflar -> Player: %p | Camera: %p | EntityManager: %p", 
                  g_player_class, g_camera_class, g_entity_manager_class);
 
-            // Döngü test thread'i
             while (true) {
                 RunESPloop();
                 sleep(1);
